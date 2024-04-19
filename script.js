75062f18a672b63c7f0595a819b6a30a9058f7c0
LucreciaC <lucre54@yahoo.com.ar>
2024-04-19 14:34:08 -0300

    Revert "Funcionamiento de Buscador y Estetica"
    This reverts commit 8ae98900612f288dc159d54196b71f2b1ff7f74f.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//LLama a la api
 const cargarDatos = async () => {
     try {
         const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
@@ -11,7 +10,7 @@ const cargarDatos = async () => {
     }
 };
 
-// Función para traducir texto con Google Translate (Rocío)
+// Función para traducir texto con Google Translate
 async function translateText(text) {
     const response = await fetch(`https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|es`);
     const data = await response.json();
@@ -33,21 +32,20 @@ async function displayMenu(menuData) {
             </tr>
         </thead>
         <tbody>
-    `
+    `;
       
     for (const item of menuItems) {
-         // Hacen que los ingredientes sean una lista 
+         // Filtrar ingredientes para eliminar los que son null
          const ingredients = Object.keys(item)
          .filter(key => key.startsWith('strIngredient') && item[key] !== null)
          .map(key => item[key]);
      
-         // Hace un salto de linea
+         // Unir los ingredientes en una cadena separada por espacios
          const ingredientList = ingredients.join(' <br>');
         
-         // Lama a la funcion de traducir
+         // Traducir las instrucciones a español
          const translatedInstructions = await translateText(item.strInstructions);
         
-         //Rellena la tabla 
         html += `
             <tr>
                 <td>${item.strDrink}</td>
@@ -65,21 +63,3 @@ async function displayMenu(menuData) {
 
 // Llama a la función cargarDatos y muestra el menú
 cargarDatos().then(datos => displayMenu(datos));
-
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('search-form');
-    form.addEventListener('submit', function(e) {
-        e.preventDefault(); // Previene el envío del formulario
-        const searchTerm = document.getElementById('search-input').value.toLowerCase();
-        buscarTragos(searchTerm);
-    });
-});
-
-async function buscarTragos(searchTerm) {
-    const datos = await cargarDatos(); // Asume que esta función devuelve todos los datos
-    const resultadosFiltrados = datos.drinks.filter(drink =>
-        drink.strDrink.toLowerCase().includes(searchTerm)
-    );
-    console.log('Resultados filtrados:', resultadosFiltrados);  // Ver resultados filtrados
-    displayMenu({ drinks: resultadosFiltrados });
-}
